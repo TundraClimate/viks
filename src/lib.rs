d4c08e41413c58e8de78ad7ec01d5cd2c0024aea
TundraClimate <tundra.tundere@gmail.com>
2025-06-26 10:57:16 +0900

    ✨feat: Impl KeyModifier

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+#![warn(missing_docs)]
+
 //! viks is parser for vim-like key sequence.  
 //! e.g. the *noremap line has unique keymap syntax in `.vimrc`.
 //!
@@ -36,3 +38,53 @@
 //! }
 //! # }
 //! ```
+
+/// Minimum unit to use for parsing.
+///
+/// Includes [KeyCode] and [KeyModifiers].  
+pub struct Key {}
+
+#[repr(u8)]
+#[derive(Clone, Copy, PartialEq)]
+/// flag of key modifier.
+///
+/// It is not recommended to use it alone.  
+/// [Key] uses with [KeyModifiers] on wrapped.
+///
+/// ## Example
+/// ```
+/// use viks::KeyModifier;
+///
+/// # fn main() {
+/// let modifier = KeyModifier::Shift | KeyModifier::Alt;
+/// # }
+/// ```
+pub enum KeyModifier {
+    /// Shift modifier
+    Shift = 0b0001,
+
+    /// Control modifier
+    Control = 0b0010,
+
+    /// Alt modifier
+    Alt = 0b0100,
+
+    /// None modifier
+    None = 0b0000,
+}
+
+impl std::ops::BitAnd for KeyModifier {
+    type Output = KeyModifier;
+
+    fn bitand(self, rhs: Self) -> Self::Output {
+        unsafe { std::mem::transmute(self as u8 & rhs as u8) }
+    }
+}
+
+impl std::ops::BitOr for KeyModifier {
+    type Output = KeyModifier;
+
+    fn bitor(self, rhs: Self) -> Self::Output {
+        unsafe { std::mem::transmute(self as u8 | rhs as u8) }
+    }
+}
