fab3da1dfa1c963f7c000c518ac92ed6a68d1c4c
TundraClimate <tundra.tundere@gmail.com>
2025-06-26 21:29:52 +0900

    ✨feat: Impl KeyCode

--- src/lib.rs
+++ src/lib.rs
@@ -44,6 +44,309 @@
 /// Includes [KeyCode] and [KeyModifiers].  
 pub struct Key {}
 
+#[repr(u8)]
+#[derive(Clone, Copy)]
+/// Visualized ascii code of char.
+///
+/// It is not recommended to uses for other than parse.
+pub enum KeyCode {
+    /// `Backspace`  
+    /// u0008
+    Backspace = 8,
+
+    /// `Tab`  
+    /// u0009
+    Tab = 9,
+
+    /// `Enter`  
+    /// u0013
+    Enter = 13,
+
+    /// `Esc`  
+    /// u01B
+    Esc = 27,
+
+    /// `Space`  
+    /// u0020
+    Space = 32,
+
+    /// `!`  
+    /// u0021
+    ExclamationMark = 33,
+
+    /// `"`  
+    /// u0022
+    QuotationMark = 34,
+
+    /// `#`  
+    /// u0023
+    NumberSign = 35,
+
+    /// `$`  
+    /// u0024
+    DollarSign = 36,
+
+    /// `%`  
+    /// u0025
+    PercentSign = 37,
+
+    /// `&`  
+    /// u0026
+    Ampersand = 38,
+
+    /// `'`  
+    /// u0027
+    Apostrophe = 39,
+
+    /// `(`  
+    /// u0028
+    LeftParenthesis = 40,
+
+    /// `)`  
+    /// u0029
+    RightParenthesis = 41,
+
+    /// `*`  
+    /// u002A
+    Asterisk = 42,
+
+    /// `+`  
+    /// u002B
+    PlusSign = 43,
+
+    /// `,`  
+    /// u002C
+    Comma = 44,
+
+    /// `-`  
+    /// u002D
+    HyphenMinus = 45,
+
+    /// `.`  
+    /// u002E
+    FullStop = 46,
+
+    /// `/`  
+    /// u002F
+    Solidus = 47,
+
+    /// `0`  
+    /// u0030
+    Digit0 = 48,
+
+    /// `1`  
+    /// u0031
+    Digit1 = 49,
+
+    /// `2`  
+    /// u0032
+    Digit2 = 50,
+
+    /// `3`  
+    /// u0033
+    Digit3 = 51,
+
+    /// `4`  
+    /// u0034
+    Digit4 = 52,
+
+    /// `5`  
+    /// u0035
+    Digit5 = 53,
+
+    /// `6`  
+    /// u0036
+    Digit6 = 54,
+
+    /// `7`  
+    /// u0037
+    Digit7 = 55,
+
+    /// `8`  
+    /// u0038
+    Digit8 = 56,
+
+    /// `9`  
+    /// u0039
+    Digit9 = 57,
+
+    /// `:`  
+    /// u003A
+    Colon = 58,
+
+    /// `;`  
+    /// u003B
+    Semicolon = 59,
+
+    /// `<`  
+    /// u003C
+    LessThanSign = 60,
+
+    /// `=`  
+    /// u003D
+    EqualSign = 61,
+
+    /// `>`  
+    /// u003E
+    GreaterThanSign = 62,
+
+    /// `?`  
+    /// u003F
+    QuestionMark = 63,
+
+    /// `@`  
+    /// u0040
+    CommercialAt = 64,
+
+    /// `A`  
+    /// u0041
+    A = 65,
+
+    /// `B`  
+    /// u0042
+    B = 66,
+
+    /// `C`  
+    /// u0043
+    C = 67,
+
+    /// `D`  
+    /// u0044
+    D = 68,
+
+    /// `E`  
+    /// u0045
+    E = 69,
+
+    /// `F`  
+    /// u0046
+    F = 70,
+
+    /// `G`  
+    /// u0047
+    G = 71,
+
+    /// `H`  
+    /// u0048
+    H = 72,
+
+    /// `I`  
+    /// u0049
+    I = 73,
+
+    /// `J`  
+    /// u004A
+    J = 74,
+
+    /// `K`  
+    /// u004B
+    K = 75,
+
+    /// `L`  
+    /// u004C
+    L = 76,
+
+    /// `M`  
+    /// u004D
+    M = 77,
+
+    /// `N`  
+    /// u004E
+    N = 78,
+
+    /// `O`  
+    /// u004F
+    O = 79,
+
+    /// `P`  
+    /// u0050
+    P = 80,
+
+    /// `Q`  
+    /// u0051
+    Q = 81,
+
+    /// `R`  
+    /// u0052
+    R = 82,
+
+    /// `S`  
+    /// u0053
+    S = 83,
+
+    /// `T`  
+    /// u0054
+    T = 84,
+
+    /// `U`  
+    /// u0055
+    U = 85,
+
+    /// `V`  
+    /// u0056
+    V = 86,
+
+    /// `W`  
+    /// u0057
+    W = 87,
+
+    /// `X`  
+    /// u0058
+    X = 88,
+
+    /// `Y`  
+    /// u0059
+    Y = 89,
+
+    /// `Z`  
+    /// u005A
+    Z = 90,
+
+    /// `[`  
+    /// u005B
+    LeftSquareBracket = 91,
+
+    /// `\`  
+    /// u005C
+    ReverseSolidas = 92,
+
+    /// `]`  
+    /// u005D
+    RightSquareBracket = 93,
+
+    /// `^`  
+    /// u005E
+    CircumflexAccent = 94,
+
+    /// `_`  
+    /// u005F
+    LowLine = 95,
+
+    /// `` ` ``  
+    /// u0060
+    GraveAccent = 96,
+
+    /// `{`  
+    /// u007B
+    LeftCurlyBracket = 123,
+
+    /// `|`  
+    /// u007C
+    VirticalLine = 124,
+
+    /// `}`  
+    /// u007D
+    RightCurlyBracket = 125,
+
+    /// `~`  
+    /// u007E
+    Tilde = 126,
+
+    /// `Delete`  
+    /// u007F
+    Delete = 127,
+}
+
 #[derive(Clone, Copy, PartialEq)]
 /// Wrapper for [KeyModifier].
 ///
