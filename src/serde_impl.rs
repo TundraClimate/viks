7f2442cb3bbdab2ed6106b27b673e8bbd9ec7127
TundraClimate <tundra.tundere@gmail.com>
2025-06-27 18:42:12 +0900

    ✨feat: Impl serde for Key

--- src/serde_impl.rs
+++ src/serde_impl.rs
@@ -4,3 +4,22 @@
 //! ```sh
 //! viks = { version = "*", features = ["serde"] }
 //! ```
+
+impl<'de> serde::Deserialize<'de> for crate::Key {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: serde::Deserializer<'de>,
+    {
+        let s = String::deserialize(deserializer)?;
+        crate::Key::new(&s).map_err(serde::de::Error::custom)
+    }
+}
+
+impl serde::Serialize for crate::Key {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        serializer.serialize_str(&self.to_string())
+    }
+}
