49ddf6b3ecf021590e5134deadccb3b6e1eff50f
TundraClimate <tundra.tundere@gmail.com>
2025-06-27 22:14:49 +0900

    ✨feat: Impl serde for Keymap

--- src/serde_impl.rs
+++ src/serde_impl.rs
@@ -23,3 +23,22 @@ impl serde::Serialize for crate::Key {
         serializer.serialize_str(&self.to_string())
     }
 }
+
+impl<'de> serde::Deserialize<'de> for crate::Keymap {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: serde::Deserializer<'de>,
+    {
+        let s = String::deserialize(deserializer)?;
+        crate::Keymap::new(&s).map_err(serde::de::Error::custom)
+    }
+}
+
+impl serde::Serialize for crate::Keymap {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        serializer.serialize_str(&self.to_string())
+    }
+}
